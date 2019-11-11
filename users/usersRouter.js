e7281e83906ec203ac433d932c716ced37c33315
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-11 14:28:01 +0100

    :sparkles: Create middleware body for validating user and hashing password

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,14 +1,28 @@
-const express = require('express')
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const db = require('./usersModel')
+const db = require("./usersModel");
 
-const users = express.Router()
+const users = express.Router();
 
-users.get('/', (req, res) => {
-    db.get()
-    .then(users => {
-        res.status(200).json(users)
-    })
-})
+users.get("/", (req, res) => {
+  db.get().then(users => {
+    res.status(200).json(users);
+  });
+});
+
+function validateUserBody(req, res, next) {
+  const { username, password } = req.body;
+
+  if (!Object.keys(req.body).length) {
+    res.status(404).json({ error: true, message: "Missing request body" });
+  } else if (!username || !password) {
+    res.status(404).json({ error: true, message: "Missing require param" });
+  } else {
+    const hashedPassword = bcrypt.hashSync(password, 123);
+    req.valUserBody = { username, hashedPassword };
+    next()
+  }
+}
 
 module.exports = users;
