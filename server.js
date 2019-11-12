5e9fc0c00e938bed6de81529eb5b51178db9dc39
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-12 13:56:47 +0100

    :sparkles: Import and use session

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
+const session = require('express-session');
 
 const userRouter = require('./users/usersRouter')
 
@@ -9,7 +10,21 @@ server.use(express.json())
 server.use(cors())
 server.use(helmet())
 
+
+const sessionConfig = {
+    name: 'aWeirdName',
+    secret: process.env.SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60, // you need it if the cookie is to survive !!
+      secure: false, // with secure, the cookie only gets set when https !!
+      httpOnly: false,
+    },
+    resave: false,
+    saveUninitialized: false,
+  }
+
 server.use('/api/users', userRouter)
+server.use(session(sessionConfig));
 
 server.get('/', (req, res) => {
     res.send('Works')
